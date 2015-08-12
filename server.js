f828b73436bbfa6a23d69c362895c969e76aab2c
panzhangwang <pz_wang@hotmail.com>
2015-08-12 23:06:14 +0800

    update

--- server.js
+++ server.js
@@ -4,21 +4,9 @@
 
 var fs = require('fs');
 var express = require('express');
-var mongoose = require('mongoose');
 var config = require('./config/config');
-
 var app = express();
 var port = process.env.PORT || 3000;
-
-// Connect to mongodb
-var connect = function () {
-  var options = { server: { socketOptions: { keepAlive: 1 } } };
-  mongoose.connect(config.db, options);
-};
-connect();
-
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
 
 // Bootstrap application settings
 require('./config/express')(app);
